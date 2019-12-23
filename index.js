5c167c4772992107491b92369cab2dd0c5c67a6f
Shivam Srivastava <percyj456@gmail.com>
2019-12-24 02:20:18 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const exphbs  = require('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+//Set Handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+const otherstuff = "Hello, this is some serious **it.";
+
+//Set Handlebar routes
+app.get('/', function (req, res) {
+    res.render('home',{
+    	stuff: otherstuff
+    });
+});
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen(PORT, () => console.log('Server listening on ' + PORT));
